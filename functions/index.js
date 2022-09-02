cb48475eb46c4f0d628072a1f6261118e5ca9417
makjac <56642363+makjac@users.noreply.github.com>
2022-09-02 02:18:38 +0200

    setup project

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,27 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://hotelmotel-66527-default-rtdb.europe-west1.firebasedatabase.app"
+});
+
+
+const express = require("express");
+const cors = require("cors");
+
+//main
+const app = express();
+app.use(cors({origin : true}));
+
+//Routers
+app.get("/", (req, res) => {
+    return res.status(200).send("Hello world!, Hotel Motel here!");
+});
+
+//export the api to firebase cloud function
+exports.app = functions.https.onRequest(app);
+
